59e283fbaa39112a615a4fab49b61a15f692727d
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-02-05 17:54:39 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,7 +9,7 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    self.song.id.sum
+    self.song.amount
   end
 
   def genre_count
